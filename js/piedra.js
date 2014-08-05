178dbee277f9391d6328aa35f759a84bb3af1979
davidherzlos <davidherzlos@gmail.com>
2014-08-05 18:34:54 -0500

    Primer commit de programación básica

---
+++ js/piedra.js
@@ -0,0 +1,31 @@
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+
+var opcionUsuario;
+var opcionMaquina;
+
+//Opcion elegida del usuario
+
+prompt("Elige 1,2 o 3");
+
+if(opcionUsuario == piedra){
+	alert("Elegiste piedra");
+}
+else if (opcionUsuario == papel){
+	alert("Elegiste papel");
+} else{
+	alert("Elegiste tijera");
+}
+
+if(opcionMaquina == piedra){
+	alert("Elegiste piedra");
+}
+else if (opcionMaquina == papel){
+	alert("Elegiste papel");
+} else{
+	alert("Elegiste tijera");
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hola mundo");
+
+var nombre = "David";
+var apellido = "Ordóñez";
+var edad = 28;
