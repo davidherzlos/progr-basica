eeb038e0d17fb21038faf5a64a79b64aa80240aa
davidherzlos <davidherzlos@gmail.com>
2015-04-20 13:31:21 -0500

    Comenzando el juego del ahorcado

---
+++ tifis/tifis-op.js
@@ -0,0 +1,99 @@
+var tablero;
+var posicion = [ "atras", "frente", "izq", "der" ];
+var rumbo;
+var fondo;
+var tifis;
+var liz;
+var direccion;
+var	posicionX = 100;
+var	posicionY = 100;
+var tecla = {
+	up: 38,
+	down: 40,
+	left: 37,
+	right: 39
+};
+function Fondo () {
+	this.URL = "fondo.png";
+	this.OK = false;
+	this.imagen = new Image();
+	this.imagen.src = this.URL;
+	this.x = 0;
+	this.y = 0;
+	this.dibujar = function(){
+		this.OK = true;
+		if (this.OK == true) {
+		tablero.drawImage(fondo.imagen, 0, 0);
+		};
+	};
+};
+function Tifis (postura) {
+
+	this.URL = "diana-" + postura + ".png";
+	this.OK = false;
+	this.imagen = new Image();
+	this.imagen.src = this.URL;
+	this.dibujar = function(){
+		this.OK = true;
+		if (this.OK == true) {
+		tablero.drawImage(tifis.imagen, tifis.x, tifis.y);
+		};
+	};
+};
+function Liz () {
+
+	this.URL = "liz.png";
+	this.OK = false;
+	this.imagen = new Image();
+	this.imagen.src = this.URL;
+	this.x = 400;
+	this.y = 300;
+	this.dibujar = function(){
+		this.OK = true;
+		if (this.OK == true) {
+		tablero.drawImage(liz.imagen, liz.x, liz.y);
+		};
+	};
+};
+function renderDibujo (rumbo) {
+
+    var canvas = document.getElementById("campo");
+	tablero = canvas.getContext("2d");
+	
+	fondo = new Fondo();
+	
+	tifis = new Tifis(rumbo || "frente");
+	Tifis.prototype.x = posicionX;
+	Tifis.prototype.y = posicionY;
+	
+	liz = new Liz();
+	
+	fondo.imagen.onload = fondo.dibujar;
+	tifis.imagen.onload = tifis.dibujar;
+	liz.imagen.onload = liz.dibujar;
+
+};
+document.addEventListener("keydown", capturarFlecha);
+
+function capturarFlecha (keypressed){ // Averiguar porqué pude acceder a un parámetro que no se que es
+
+	tecla.presionada = keypressed.keyCode;
+
+	if (tecla.presionada == tecla.up) {
+		posicionY -= 10;
+		rumbo = posicion[0];
+	};
+	if (tecla.presionada == tecla.down) {
+		posicionY += 10;
+		rumbo = posicion[1];
+	};
+	if (tecla.presionada == tecla.left) {
+		posicionX -= 10;
+		rumbo = posicion[2];
+	};
+	if (tecla.presionada == tecla.right) {
+		posicionX += 10;
+		rumbo = posicion[3];
+	};
+	renderDibujo(rumbo);
+};
--- tifis/tifis.js
+++ tifis/tifis.js
@@ -1,11 +1,19 @@
-
-console.log('funciona JS');
+console.log('Funciona el código JS');
 
 // Variables globales, no precisamente una buena práctica
 
-var tablero;
+var tablero, direccion;
 
-// Objetos fondo, Diana y liz...
+var ascii = [38, 40, 37, 39];
+
+var tecla = {
+	up: ascii[0],
+	down: ascii[1],
+	left: ascii[2],
+	right: ascii[3]
+};
+
+// Fondo, Tifis(Diana) y liz...
 
 var fondo = {
 	imagenURL: "fondo.png",
@@ -16,33 +24,42 @@ var tifis = {
 	x: 100,
 	y: 100,
 	frenteURL: "diana-frente.png",
-	frenteOK: false
+	atrasURL: "diana-atras.png",
+	izquierdaURL: "diana-izq.png",
+	derechaURL: "diana-der.png",
+	frenteOK: false,
+	atrasOK: false,
+	derechaOK: false,
+	izquierdaOK:false
 };
 
 var liz = {
+	lizURL: "liz.png",
+	lizOK: false,
+	x: 400,
+	y: 200
 };
 
 
 function funcionInicio () {
-	console.log('Listo para dibujar');
+	console.log('Una vez que el DOM cargó');
 
-	// Obtener el canvas
-	var canvas = document.getElementById("campo");
+// Obtener el canvas
+    var canvas = document.getElementById("campo");
 	//Obtener contexto del canvas
 	tablero = canvas.getContext("2d");
 
-
-
-	// definir la imagen agregando propiedades a fondo
+// definir la imagen agregando un nuevo objeto de imagen
 	fondo.imagen = new Image();
 		//Comprobación
 		console.log(fondo);
-	//Asignar la imagen al atributo src de objeto imagen
+	//Asignar la url de la imagen a la imagen recien creada
 	fondo.imagen.src = fondo.imagenURL;
 		//Comprobación
-		console.log(fondo);
-	// Ojo, aún no la hemos insertado en este punto, aquí aún no se ve la foto, sólo ocupa esta técnica, porque la carga de imágenes es un poco mas complicado
-	// Este es un evento  IMPORTANTE!!
+		console.log(fondo); // Sólo comprobamos que el objeto ya está listo para poder operar sobre el
+
+	// Ojo, aún no la hemos insertado en este punto, aquí aún no se ve la foto
+	// Este es un evento  IMPORTANTE!! Necesitamos estar seguros de que la imagen cargó
 	fondo.imagen.onload = confirmarFondo; //La función se dispara cuando la imagen carga. La función sólo se referencía, no debe llevar parántesis
 
 // Definir la imagen de tifis agregandole propiedades
@@ -51,58 +68,113 @@ function funcionInicio () {
 	tifis.frente.src = tifis.frenteURL;
 	tifis.frente.onload = confirmarFrente;
 
+	tifis.atras = new Image();
+	tifis.atras.src = tifis.atrasURL;
+	tifis.atras.onload = confirmarAtras;
 
-var m = document.getElementById("mover");
-m.addEventListener("click", movimiento)
+
+	tifis.izquierda = new Image();
+	tifis.izquierda.src = tifis.izquierdaURL;
+	tifis.izquierda.onload = confirmarIzquierda;
+
+
+	tifis.derecha = new Image();
+	tifis.derecha.src = tifis.derechaURL;
+	tifis.derecha.onload = confirmarDerecha;
+
+
+
+// Definir la imagen de liz agregandole propiedades
+
+	liz.lizy = new Image();
+	liz.lizy.src = liz.lizURL;
+	liz.lizy.onload = confirmarLiz;
+
+// Vamos a añardir un evento para disparar una función al apretar una tecla del teclado
+
+	document.addEventListener("keydown", capturarFlecha);
+
+
 
 };
 
-function confirmarFrente () {
-	tifis.frenteOK = true;
+// Las funciones llamadas si el evento onload ocurre:
+
+function confirmarLiz () {
+	liz.lizOK = true;
 	dibujar();
 };
 
-
-function movimiento() {
-	tifis.x += 10;
-	dibujar();
-};
-
-
-
-
-
-// Cuando la imagen es true, ahora si puedo dibujar, estamos confirmando el fondo
 function confirmarFondo(){
 	fondo.imagenOK = true;
 	dibujar();
 
 };
 
-function dibujar () {
+function confirmarAtras () {
+	tifis.atrasOK = true;
+}
+
+function confirmarFrente () {
+	tifis.frenteOK = true;
+	dibujar();
+}
+
+function confirmarIzquierda () {
+	tifis.izquierdaOK = true;
+}
+
+function confirmarDerecha () {
+	tifis.derechaOK = true;
+}
+
+function capturarFlecha (keypressed) { // Esto está muy raro, debes investigar cómo son las funciones oara saber qué pasó aquí
+	// Guardar en código la tecla oprimida
+	
+	var tecla_elegida = keypressed.keyCode;
+	
+	dibujar(tecla_elegida);
+};
+
+
+function dibujar (tecla_elegida) {
 	// Esto agrega ahora sí la imagen, tres parámetros, imagen, posición.
 
+	// Capa 1
 	if (fondo.imagenOK == true) {
 		tablero.drawImage(fondo.imagen, 0, 0); //Es una función propia de canvas
 	};
 
-
-	if (tifis.frenteOK == true) {
-		tablero.drawImage(tifis.frente, tifis.x, tifis.y);
+	// Capa 2
+	if (liz.lizOK == true) {
+		tablero.drawImage(liz.lizy, liz.x, liz.y);
 	};
+	
+	// Capa 3
+	var tifisDibujo = tifis.frente;
+
+	if (tifis.frenteOK && tifis.atrasOK && tifis.derechaOK && tifis.izquierdaOK) {
+
+		if (tecla_elegida == tecla.up){
+			tifis.y -= 10;
+			tifisDibujo = tifis.atras;
+		}
+		if (tecla_elegida == tecla.down){
+			tifis.y += 10;
+			tifisDibujo = tifis.frente;
+		}
+		if (tecla_elegida == tecla.left){
+			tifis.x -= 10;
+			tifisDibujo = tifis.izquierda;
+		}
+		if (tecla_elegida == tecla.right){
+			tifis.x += 10;
+			tifisDibujo = tifis.derecha;
+		}
+	}
+	
+	tablero.drawImage(tifisDibujo, tifis.x, tifis.y);
 };
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Hay que volver a dibujar el osito
