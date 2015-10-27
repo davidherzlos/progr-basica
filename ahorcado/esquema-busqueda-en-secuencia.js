8bb3b97a671498d93315b09a6b4578266ec1a5f6
davidherzlos <davidherzlos@gmil.com>
2015-10-26 19:38:23 -0600

    Apuntes algorítmica

--- ahorcado/esquema-busqueda-en-secuencia.js
+++ ahorcado/esquema-busqueda-en-secuencia.js
@@ -1,37 +1,18 @@
-// Buen ejemplo de esquema de búsqueda en una secuencia
+/*Buen ejemplo del esquema de búsqueda en una secuencia*/
 
+// 	Variables
 var palabras = ["uno", "dos", "tres", "cuatro"];
 var palabraBuscada = prompt("Dame una palabra");
-var i = 0;
-var encontrado = false;
-while(i <= palabras.length -1 && encontrado == false){
-
-
-	if (palabraBuscada == palabras[i]) {
-		console.log("Palabra encontrada en la posición " + i + " con valor: " + palabras[i]);
-		encontrado = true;
-	};
-	if(encontrado == false){
-		console.log("Palabra NO encontrada en la posición " + i + " con valor: " + palabras[i]);
-		i++;
-	}
-
-}
-
-
-// COMENTADA
-
-// Datos de entrada
-var palabras = ["uno", "dos", "tres", "cuatro"];
-var palabraBuscada = prompt("Dame una palabra");
+var i;
+var encontrado;
 
 // Preparar  secuencia
-var i = 0;
+i = 0;
+encontrado = false;
 
-// Encontrado igual a falso
-var encontrado = false;
+//Iniciar tratamiento (en este caso no aplica, pues no tratamos inicialmente nada)
 
-// Mientras no sea fin de secuencia y no se haya encontrado
+// Mientras no sea fin de secuencia y no se haya encontrado en elemento
 while(i <= palabras.length -1 && encontrado == false){
 
 	//Actualizar encontrado
@@ -45,5 +26,31 @@ while(i <= palabras.length -1 && encontrado == false){
 		console.log("Palabra NO encontrada en la posición " + i + " con valor: " + palabras[i]);
 		i++;
 	}
-
 }
+
+// Tratamiento Final
+console.log("La palabra " + palabraBuscada + (encontrado == true ? " sí" : " no") + " existe en la secuencia");
+
+
+
+
+
+/****Sin comentar****/
+
+var palabras = ["uno", "dos", "tres", "cuatro"];
+var palabraBuscada = prompt("Dame una palabra");
+var i;
+var encontrado;
+i = 0;
+encontrado = false;
+while(i <= palabras.length -1 && encontrado == false){
+	if (palabraBuscada == palabras[i]) {
+		console.log("Palabra encontrada en la posición " + i + " con valor: " + palabras[i]);
+		encontrado = true;
+	};
+	if(encontrado == false){
+		console.log("Palabra NO encontrada en la posición " + i + " con valor: " + palabras[i]);
+		i++;
+	}
+}
+console.log("La palabra " + palabraBuscada + (encontrado == true ? " sí" : " no") + " existe en la secuencia");
