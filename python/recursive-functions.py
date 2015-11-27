ebc387621d9b07fbe539beea991a5d048ef46c65
davidherzlos <davidherzlos@gmil.com>
2015-11-26 20:31:01 -0600

    recursivas

--- python/recursive-functions.py
+++ python/recursive-functions.py
@@ -1,3 +1,8 @@
+""" Para toda función recursiva, hay que considerar lo siguiente:
+1. Definir la función, sus parámetros y su valor de retorno
+2. Escriir un caso base en donde la función recursiva deje de serlo, casi siempre un if
+3. Hacer la función recursiva pasándole parámetros menores a los de lal última llamada a la función, para asegurar que se avance en la secuencia
+4. Probar y probar """
 
 def suma(n):
     print("En este momento n vale", n) 
@@ -9,6 +14,6 @@ def suma(n):
 
 
 x = int(input("Escribe un número"))
-s = suma(xn
+s = suma(x)
 
 print("La suma es", s)
